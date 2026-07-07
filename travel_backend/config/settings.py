c84edc2f4b0b219627757e18f904d3e761b9b1a0
zeeya <ziyastha@gmail.com>
2026-07-07 23:11:21 +0545

    postgres

--- travel_backend/config/settings.py
+++ travel_backend/config/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # =========================
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travel_db',
+        'USER': 'travel_user',
+        'PASSWORD': 'travel@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
